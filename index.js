9417bb262dbddda7ff757075ee563c9b75541f4a
Evelyne <e.umubyeyi@alustudent.com>
2023-02-02 10:09:30 +0200

    Added close popup functionality

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let popup = document.getElementById("skills_popup")
 let skillCategories = document.getElementsByClassName("skill_category")
-
+let closePopup = document.getElementById("close_popup") 
+ 
 function openSkillsPopup(){
     console.log("Clicked")
     // popup.classList.add("show_popup");
     popup.style.visibility = "visible";
 }
 
+function closePopupFn(){
+    popup.style.visibility = "hidden";
+}
+
+closePopup.addEventListener('click',closePopupFn)
+
 for (let i = 0; i < skillCategories.length; i++) {
     skillCategories[i].addEventListener('click', openSkillsPopup);
 }
+
